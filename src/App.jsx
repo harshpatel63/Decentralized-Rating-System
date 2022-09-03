ba1ca09c8a8f432d9d6313171ae4a08816c1904c
thisisbipin <bipinmjadav087@gmail.com>
2022-09-03 16:01:37 +0530

    product page done

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Products from "./components/Products/Products";
-import ProductsPage from "./components/Products/ProductsPage";
+import ProductPage from "./components/Products/ProductPage";
 import Home from "./Home";
 
 let productsList = [];
@@ -18,7 +18,7 @@ function App() {
                     <Route path="/products" element={<Products />} />
                     <Route
                         path="/product/:id"
-                        element={<ProductsPage productsList={productsList} />}
+                        element={<ProductPage productsList={productsList} />}
                     />
                 </Routes>
 
--- src/components/Products/ProductPage.jsx
+++ src/components/Products/ProductPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import "./css/ProductPage.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import ReviewFetch from "./ReviewFetch";
 
-function ProductsPage() {
+function ProductPage() {
+    let [reviewFetched, setreviewFetched] = useState(true);
     let state = {
         currentproduct: {
             PID: "1",
@@ -11,6 +15,8 @@ function ProductsPage() {
             Details:
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         },
+        points: 0,
+        count: 1,
     };
     /*
     //Loading spinner
@@ -52,8 +58,40 @@ function ProductsPage() {
                     </div>
                 </div>
             </section>
+            <section className="productdetail__product-review-area">
+                <div className="productdetail__product-review-area__container">
+                    <div className="productdetail__product-review-area__container__content">
+                        <div className="productdetail__product-review-area__container__content__reviews__overallreview">
+                            <div className="overallreview-container">
+                                <h5>Overall</h5>
+                                <h4>
+                                    {reviewFetched === true
+                                        ? state.count == 0
+                                            ? 0
+                                            : (
+                                                  state.points / state.count
+                                              ).toFixed(2)
+                                        : LoadingSpinner(false)}
+                                </h4>
+                                <h6>({state.count} Reviews)</h6>
+                            </div>
+                        </div>
+                        <ReviewFetch />
+                    </div>
+                </div>
+                lol
+            </section>
         </>
     );
 }
-
-export default ProductsPage;
+export function LoadingSpinner(heading = true) {
+    return (
+        <div className="productdetail__loader">
+            {heading == true && <h1>Loading...</h1>}{" "}
+            <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    );
+}
+export default ProductPage;
